d1d913cea5c9fda2e97f46b0a178db749468f68a
DenisCo1100100 <kodek_den2001@mail.ru>
2020-09-16 05:58:36 +0300

    Project creation. Written script

---
+++ Assets/Scripts/CarControll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 moveLeftVector;
+    public Vector3 moveRightVector;
+    public float speed;
+
+    void Start()
+    {    
+    }
+
+    void Update()
+    {
+        Vector3 movePositions = new Vector3(target.position.x, 1.18f, -5.70f);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (target.transform.position.x > -2.8f)
+            {
+                target.transform.Translate(moveLeftVector * Time.deltaTime * speed);
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (target.transform.position.x < 2.8f)
+            {
+                target.transform.Translate(moveRightVector * Time.deltaTime * speed);
+            }
+        }
+
+        transform.LookAt(target);
+        transform.position = Vector3.MoveTowards(transform.position, movePositions, Time.deltaTime * (speed - 0.5f));
+    }
+}
---
+++ Assets/Scripts/SpawnerObjects.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerObjects : MonoBehaviour
+{
+    public bool isSameDelay;
+    public bool isRanomObject;
+    public GameObject objectToSpawn;
+    public GameObject[] objectsToSpawn;
+    public float timeBeforeSpawn;
+    public float spawnDelay;
+    public float minDelay;
+    public float maxDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isSameDelay)
+        {
+            InvokeRepeating("Spawn", timeBeforeSpawn, spawnDelay);
+        }
+        else
+        {
+            
+        }
+    }
+
+    private IEnumerator Spawner()
+    {
+        yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+        Spawner();
+    }
+
+    private void Spawn()
+    {
+        
+        if (isRandomObject)
+        {
+           GameObkect obj = Instantiate(ObjectsToSpwan[Random.Range(0, ObjectsToSpwan.Length)], transform.position, transform.rotation) as GameObject;
+        }
+        if (!isRanomObject)
+        {
+            GameObject obj = Instantiate(objectToSpawn, transform.position, transform.rotation) as GameObject;
+        }
+        else if (!isSameDelay)
+        {
+            StartCorotine(Spawner());
+        }
+    } 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
