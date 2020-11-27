9904342f40a604fc1f03460a9059cf06a12bb328
DenisCo1100100 <kodek_den2001@mail.ru>
2020-11-27 21:07:05 +0300

    Added sound change in main menu

--- Assets/Scripts/UIScripts/SetingMenu.cs
+++ Assets/Scripts/UIScripts/SetingMenu.cs
@@ -5,15 +5,19 @@ using UnityEngine.UI;
 
 public class SetingMenu : MonoBehaviour
 {
-    Slider musicVolume;
+    [SerializeField] private GameObject mainMenu;
+    private AudioSource menuSound;
+    private Slider musicVolume;
 
     private void Awake()
     {
         musicVolume = gameObject.GetComponentInChildren<Slider>();
+        menuSound = mainMenu.GetComponent<AudioSource>();
     }
 
     private void Update()
     {
         StaticMenagerSetings.volume = musicVolume.value;
+        menuSound.volume = musicVolume.value;
     }
 }
